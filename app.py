3e640494824d2cf8269dbf449e2e24b928f9db3c
m-spunky <mayurdjadhav1@gmail.com>
2024-03-23 19:04:47 +0530

    interface

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print("Ganapati Bappa Morya")
+import os
+from pathlib import Path
+import streamlit as st
+
+
+def main():
+    st.title("Resume Parser and Job Matcher")
+
+    with st.sidebar:
+        st.subheader("Settings")
+        openai_key_txt = st.text_input("OpenAI Token", placeholder="OpenAI token or env[OPENAI_API_KEY]")
+
+
+    # Resume parsing section
+    st.subheader("Resume Parser")
+    pdf_file = st.file_uploader("Upload a PDF file for parsing")
+    if pdf_file is not None:
+        pass
+
+
+    # Job matching section
+    st.subheader("Job Matcher")
+    job_text = st.text_area("Enter the job description")
+    if job_text:
+        pass
+    
+if __name__ == "__main__":
+    main()
